13932d0c2d5b8b51895fbe6571462accbfbebf53
Elkina Varvara <elkinavarvara1999@gmail.com>
2025-10-15 15:47:00 +0300

    day 2: Receiving data from the server: a complex task

---
+++ js/getGoods.js
@@ -0,0 +1,24 @@
+const getGoods = () => {
+    const links = document.querySelectorAll('.navigation-link')
+
+    const getData = () => {
+        fetch('https://wildberris-159d3-default-rtdb.firebaseio.com/db.json')
+        .then((res) => res.json())
+        .then((data) => {
+            localStorage.setItem('goods', JSON.stringify(data))
+        })
+    }
+
+
+    links.forEach((link) => {
+        link.addEventListener('click',(event) => {
+            getData()
+        })
+    })
+
+    localStorage.setItem('goods', JSON.stringify({
+        name: 'all'
+    }))
+}
+
+getGoods()
